eb31d374d49ac4da0a10b115b1103b142d5acffe
josscg449 <38119088+josscg449@users.noreply.github.com>
2020-05-11 23:36:37 -0600

    Updates

---
+++ precache-manifest.ffb96ddc0d2537ee0802f1310d59d4ca.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d807b6b218d120b2ff777a03387780f",
+    "url": "/drumpad/index.html"
+  },
+  {
+    "revision": "04dae26914232c5fa11a",
+    "url": "/drumpad/static/css/main.fa527750.chunk.css"
+  },
+  {
+    "revision": "5b7c676077f0c9d39558",
+    "url": "/drumpad/static/js/2.267a8790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drumpad/static/js/2.267a8790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dae26914232c5fa11a",
+    "url": "/drumpad/static/js/main.d37fe4a7.chunk.js"
+  },
+  {
+    "revision": "eca9aa59cc7c4315f4b1",
+    "url": "/drumpad/static/js/runtime-main.f1d6791c.js"
+  },
+  {
+    "revision": "7c3488a3fdbb57873abe75bb256a2247",
+    "url": "/drumpad/static/media/Ahigh.7c3488a3.wav"
+  },
+  {
+    "revision": "a0b7fe842fd6a3336c97200ff97867e7",
+    "url": "/drumpad/static/media/Alow.a0b7fe84.wav"
+  },
+  {
+    "revision": "3e94f9b526ce3c9892d4e7afac810612",
+    "url": "/drumpad/static/media/Dhigh.3e94f9b5.wav"
+  },
+  {
+    "revision": "8232f80c95c39e2db791f0894c0a731a",
+    "url": "/drumpad/static/media/Dlow.8232f80c.wav"
+  },
+  {
+    "revision": "3b0ce16711d6a3ffda9e79695257a1f3",
+    "url": "/drumpad/static/media/Ehigh.3b0ce167.wav"
+  },
+  {
+    "revision": "53eee09f9e6c0381e6824f044a3f086b",
+    "url": "/drumpad/static/media/Elow.53eee09f.wav"
+  },
+  {
+    "revision": "e5a84552b3026ff3264dcd1b2a09d9a2",
+    "url": "/drumpad/static/media/bassArhythm.e5a84552.wav"
+  },
+  {
+    "revision": "5b37326f5de12f57c93a98c7c536e2fa",
+    "url": "/drumpad/static/media/bassDrhythm.5b37326f.wav"
+  },
+  {
+    "revision": "6ee302702832f83f0b8490c289825ad1",
+    "url": "/drumpad/static/media/bassErhythm.6ee30270.wav"
+  },
+  {
+    "revision": "8c7c67c8bb7566f4734ed731abcab721",
+    "url": "/drumpad/static/media/closedhat.8c7c67c8.wav"
+  },
+  {
+    "revision": "42e9914927559d556171a7d3852ee874",
+    "url": "/drumpad/static/media/crashcym.42e99149.wav"
+  },
+  {
+    "revision": "9bab6a1b5337b57b26ff1587285f9e7f",
+    "url": "/drumpad/static/media/floortom.9bab6a1b.wav"
+  },
+  {
+    "revision": "92352c12076d39af82a4c6e9b568916a",
+    "url": "/drumpad/static/media/funky1.92352c12.wav"
+  },
+  {
+    "revision": "614cdb2b5e92479788d60214c532272c",
+    "url": "/drumpad/static/media/funky2.614cdb2b.wav"
+  },
+  {
+    "revision": "31083e526eb1bc2a23afb04c8ac9a5eb",
+    "url": "/drumpad/static/media/funky3.31083e52.wav"
+  },
+  {
+    "revision": "1d3e144da79d96330328ec14c2d4da02",
+    "url": "/drumpad/static/media/guitarA.1d3e144d.wav"
+  },
+  {
+    "revision": "696d1b246f9f0714ce59633c8ba08192",
+    "url": "/drumpad/static/media/guitarD.696d1b24.wav"
+  },
+  {
+    "revision": "14c16b21232de5993fd1f405109923da",
+    "url": "/drumpad/static/media/guitarE.14c16b21.wav"
+  },
+  {
+    "revision": "a35b617d9421891c46bb2667246b50d3",
+    "url": "/drumpad/static/media/kick.a35b617d.wav"
+  },
+  {
+    "revision": "81ddff693db22e0f8b9835d5ffca48dd",
+    "url": "/drumpad/static/media/openhat.81ddff69.wav"
+  },
+  {
+    "revision": "cca99965f17c790323a703a60edc69e1",
+    "url": "/drumpad/static/media/pianoA.cca99965.wav"
+  },
+  {
+    "revision": "0c0cf51b45198b5ebcdce22398004621",
+    "url": "/drumpad/static/media/pianoC.0c0cf51b.wav"
+  },
+  {
+    "revision": "5b928bd14f229eb17269c23a847973ee",
+    "url": "/drumpad/static/media/pianoCs.5b928bd1.wav"
+  },
+  {
+    "revision": "d8bf6d47b088a4813916ed1c2119ccd4",
+    "url": "/drumpad/static/media/pianoD.d8bf6d47.wav"
+  },
+  {
+    "revision": "148d978b64dcd224366cece56705f9b5",
+    "url": "/drumpad/static/media/pianoE.148d978b.wav"
+  },
+  {
+    "revision": "59cc4111cbde8cac21be3e79715010d5",
+    "url": "/drumpad/static/media/pianoF.59cc4111.wav"
+  },
+  {
+    "revision": "4ec1f8b89010e7d0e4f1d1f1692e1272",
+    "url": "/drumpad/static/media/pianoFs.4ec1f8b8.wav"
+  },
+  {
+    "revision": "5e0c004da690404b78305ff1d4ef3893",
+    "url": "/drumpad/static/media/pianoG.5e0c004d.wav"
+  },
+  {
+    "revision": "cd6155bcfd06b0b3f99af8ceae180ff2",
+    "url": "/drumpad/static/media/pianoGs.cd6155bc.wav"
+  },
+  {
+    "revision": "da7868c28e59f3bbe611923472c74d1b",
+    "url": "/drumpad/static/media/ridecym.da7868c2.wav"
+  },
+  {
+    "revision": "1402fa2b1c8feb15895fcf08f587ff29",
+    "url": "/drumpad/static/media/riff1.1402fa2b.wav"
+  },
+  {
+    "revision": "c41c8023ee4578371df9f1ca30ab0c07",
+    "url": "/drumpad/static/media/riff2.c41c8023.wav"
+  },
+  {
+    "revision": "656c26bf79cb63a914e2ee0af55d42ec",
+    "url": "/drumpad/static/media/riff3.656c26bf.wav"
+  },
+  {
+    "revision": "41cfdd911aad5544694007de5b99ae30",
+    "url": "/drumpad/static/media/rim.41cfdd91.wav"
+  },
+  {
+    "revision": "a633b0e91077113f2119e7a6661a241c",
+    "url": "/drumpad/static/media/smalltom.a633b0e9.wav"
+  },
+  {
+    "revision": "a4db20c83e6c3ccbd9b9961975f03522",
+    "url": "/drumpad/static/media/snare.a4db20c8.wav"
+  }
+]);
